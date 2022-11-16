580730139811943f2b4ed4bb7ffef2822bdb8a11
Ubuntu <ubuntu@ip-172-31-9-78.ap-northeast-2.compute.internal>
2022-11-16 07:27:48 +0000

    api_gw_start.sh 수정

--- bin/api_gw_start.sh
+++ bin/api_gw_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
   
 echo "api_gw 시작"
-nohup python -u ../src/api_gw.py &
+nohup python3 -u ../src/api_gw.py &
 echo "---------------------------------------------"
 ps -ef | grep api_gw | grep -v grep
 echo "---------------------------------------------"
